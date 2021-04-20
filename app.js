791c8c1969fd81e49d154a84cb6247c73e5bc563
icld <ian@icldesign.com>
2021-04-20 13:02:58 -0700

    reconfig routes

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const app = express()
 // const path = require('path')
-const data = require('./data/data.json');
-const projects = data.projects
+
+const mainRoutes = require('./routes')
+
+
+app.use(mainRoutes)
 
 app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    res.render('index')
-});
 
-app.get('/about', (req, res) => {
-    res.render('about')
-});
 
-app.get('/:id', (req, res) => {
-    const id = req.params.id
-    const project = projects[id]
-    res.render('project', { project })
-});
+
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
---
+++ routes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+const data = require('./data/data.json');
+const projects = data.projects
+
+
+
+router.get('/', (req, res) => {
+
+    res.render('index', { projects })
+});
+
+router.get('/about', (req, res) => {
+    res.render('about')
+});
+
+router.get('/projects/:id', (req, res) => {
+    const num = parseFloat(req.params.id)
+
+    const project = data.projects.find(proj => proj.id === num)
+
+    if (project) {
+        console.log(project)
+        res.render('project', { project })
+    } else {
+        console.error('Something is wrong')
+        res.redirect('/projects/1')
+    }
+
+
+});
+
+
+
+
+module.exports = router
